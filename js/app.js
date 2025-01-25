575998f1cab3873f7210e36c7d010b6d17fc3331
Allan Kardec <allantwister007@gmail.com>
2025-01-25 02:09:41 -0300

    Confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@ function alterarStatus(id){
     let game = document.getElementById(`game-${id}`);
     let imagem = game.querySelector('.dashboard__item__img');
     let botao = game.querySelector('.dashboard__item__button');
+    let nomeJogo = game.querySelector('.dashboard__item__name');
    
     if (imagem.classList.contains('dashboard__item__img--rented')){
+      if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+      }
     }else{
         imagem.classList.remove(botao);
         imagem.classList.add('dashboard__item__img--rented');
